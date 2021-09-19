0e64ba2a022c7db6b62aadcccce045e117be46d7
quickitchen-ma <contact@quickitchen.ma>
2021-09-19 20:07:33 +0200

    added firebase id

--- web/firebase-messaging-sw.js
+++ web/firebase-messaging-sw.js
@@ -2,12 +2,12 @@ importScripts("https://www.gstatic.com/firebasejs/7.20.0/firebase-app.js");
 importScripts("https://www.gstatic.com/firebasejs/7.20.0/firebase-messaging.js");
 
 firebase.initializeApp({
-  apiKey: "AIzaSyBRobgZqIC-dFsr05MzxUQXxQITjKpnDH0",
-  authDomain: "emarket-e420c.firebaseapp.com",
-  projectId: "emarket-e420c",
-  storageBucket: "emarket-e420c.appspot.com",
-  messagingSenderId: "151590191214",
-  appId: "1:151590191214:web:4e7582ed290b4f60a5667f",
+  apiKey: "AIzaSyC5UrZsnrWt9rKpjXg8QDqOS7orvvYBjFY",
+  authDomain: "quickitchen-dotma.firebaseapp.com",
+  projectId: "quickitchen-dotma",
+  storageBucket: "quickitchen-dotma.appspot.com",
+  messagingSenderId: "999380152523",
+  appId: "1:999380152523:web:bfe236afa718f9ec0b2b4e",
   databaseURL: "...",
 });
 
